f0ebabbe1fe78bd0700513ab4cf4916dc940f7e9
The Luke <luke@SPStories.com>
2022-05-30 16:03:13 +0700

    fixed bug with array

--- .next/static/onBDKbYACxXwkL2RIWqZX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-29d198ba233ea834.js","static/chunks/pages/index-bcb8bd64fd8b84a4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.jsx
+++ pages/index.jsx
@@ -149,6 +149,9 @@ const Home = () => {
    */
   const showHolderAddresses = async(tokenIds, nft) => {
     setCurPage("WALLETS_FOR_NFT");
+    console.log("showHolderAddresses");
+    console.log("tokenIds=",tokenIds);
+    console.log("nft=",nft);
     
     setSpinner(true);
  
@@ -268,7 +271,7 @@ const Home = () => {
             </div>
             
             <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-              <a href="#" onClick={(e)=>{showHolderAddresses([...nftForTokenAddresses.id.tokenId], nftForTokenAddresses)}} className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"> Back To Wallet List </a>
+              <a href="#" onClick={(e)=>{showHolderAddresses([nftForTokenAddresses.id.tokenId], nftForTokenAddresses)}} className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"> Back To Wallet List </a>
             </div>
           
           </div>
